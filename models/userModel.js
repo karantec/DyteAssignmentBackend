e56b31a5e3256581280358981fd424a06e2940f7
karantech <karanrana3095@gmail.com>
2023-11-15 10:58:54 +0530

    finaldone

---
+++ models/userModel.js
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+
+
+const userSchema = new mongoose.Schema({
+    JProfile:{
+       type:String, 
+        
+    },
+    CompanyName:{
+        type: String,
+        
+    },
+    JobLocation:{
+        type: String,
+        
+    },
+   JobExperience:{
+        type: String,
+        
+    },
+    JobType:{
+        type:String,
+    },
+    JobDescription:{
+        type:String,
+    },
+    JobDescription:{
+        type:String,
+    },
+    JobSkills:{
+        type:String,
+    },
+    JobRole:{
+        type:String,
+    },
+    ApplyLink:{
+        type:String,
+    }
+})
+
+
+export default mongoose.model("User", userSchema);
